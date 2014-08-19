ee48c28a44ff2287200cd047990bee12eac7c612
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-08-19 17:09:09 +0200

    Use Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -24,3 +24,30 @@ gulp.task('less', function() {
     .pipe(less())
     .pipe(gulp.dest(paths.target));
 });
+
+// JS compiling
+gulp.task('browserify', function() {
+  return gulp.src(paths.js.entryPoint)
+    .pipe(browserify({
+      debug: true,
+      insertGlobals: false
+    }))
+    .pipe(gulp.dest(paths.target));
+});
+
+// JS linting
+gulp.task('lint', function() {
+  return gulp.src(paths.js.all)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Auto-run tasks on file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.js.all, ['lint', 'browserify']);
+  gulp.watch(paths.less.all, ['less']);
+});
+
+// Run main tasks on launch
+gulp.task('default', ['lint', 'less', 'browserify', 'watch'], function() {
+});
