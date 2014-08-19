6a3054edf79d8fb7cb5d5da904d3fe44eb114ea4
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-08-19 19:16:07 +0200

    Stubs for tests

---
+++ test/detect-context.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var detextContext = require('../assets/js/detect-context.js');
+
+describe('<Context detection>', function() {
+  var fakeContext = 'Léo Hua--Döl';
+
+  it('should recognize supported sites URLs', function() {
+
+  });
+
+  it('should return false for non-supported sites', function() {
+
+  });
+
+  it('should extract context query string on supported sites', function() {
+
+  });
+});
