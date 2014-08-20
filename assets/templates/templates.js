8d3d8786fdf8a8f7e6604b62b488cee6d163e811
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-08-20 12:26:19 +0200

    Use BRFS to inline static templates into JS compiled source

---
+++ assets/templates/templates.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var fs = require('fs');
+
+/**
+ * @file Thanks to the BRFS Browserify transform,
+ * the static resources loaded via `fs.readFileSync`
+ * end up being inlined directly into the compiled JS source.
+ */
+
+module.exports = {
+  results: fs.readFileSync(__dirname + '/results.html', 'utf8'),
+  snippet: fs.readFileSync(__dirname + '/snippet.html', 'utf8')
+};
