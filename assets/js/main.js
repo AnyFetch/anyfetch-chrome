03ce9e3789b21c0c76b12b83510dffbf87410f66
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-08-23 11:15:31 +0200

    Move lines around

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,22 +5,23 @@ var config = require('./configuration.js');
 // We need to wait on `chrome.storage` to load the user's settings
 // before doing anything else
 config.loadUserSettings(function() {
-  var errors = require('./errors.js');
-  var postUpdateIfNecessary = require('./post-update-if-necessary.js');
-  var detectContext = require('./detect-context.js');
-  var getDocuments = require('./get-documents.js');
   var view = require('./view.js');
   var badge = require('./badge.js');
+  var errors = require('./errors.js');
+
+  var detectContext = require('./detect-context.js');
+  var getDocuments = require('./get-documents.js');
+  var postUpdateIfNecessary = require('./post-update-if-necessary.js');
 
   // TODO: go through the tabs and disable the BrowserAction for each, except the supported sites
   // chrome.tabs.query(null, cb([tabs]));
-  // TODO: consider using a PageAction rather than a BrowserAction (https://developer.chrome.com/extensions/overview)
   // chrome.browserAction.disable(tab.id);
-  // TODO: look-ahead (do not wait to be clicked to fetch results)
+  // TODO: consider using a PageAction rather than a BrowserAction (https://developer.chrome.com/extensions/overview)
+  // TODO: start loading results without waiting to be clicked (using a content script?)
   // TODO: i18n
   // TODO: cache results & adjust view when switching back and forth between tabs
 
-  $(document).ready(function() {
+  var main = function() {
     if(!config.token) {
       errors.show('Please <a href="options.html" target="_blank">setup your AnyFetch account</a> to start using AnyFetch.');
       return;
@@ -47,6 +48,7 @@ config.loadUserSettings(function() {
         errors.show('No context detected.');
       }
     });
+  };
 
-  });
+  $(document).ready(main);
 });
