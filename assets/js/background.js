adcee0a565fe1721ca44211e419f199b40af78b7
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-08-25 11:53:27 +0200

    Activation rule example

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('Testing background script');
+console.log('Updating rules');
+
+/**
+ * @see https://developer.chrome.com/extensions/declarativeContent#type-PageStateMatcher
+ */
+
+// When the extension is installed or upgraded
+chrome.runtime.onInstalled.addListener(function() {
+  // Update the activation rule
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            // TODO
+            pageUrl: { urlContains: 'github' },
+          })
+        ],
+        // Action: show the PageAction
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
