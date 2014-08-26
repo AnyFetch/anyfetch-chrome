4dd00c9007917c6f12b9ec2c4ab0664804257609
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-08-26 11:05:29 +0200

    `sliceInTime` helper function (from SalesFetch)

---
+++ assets/js/helpers/slice-in-time.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var moment = require('moment');
+
+var timeSlices = [
+  {
+    label: 'Today',
+    maxDate: moment().startOf('day')
+  },
+  {
+    label: 'Yesterday',
+    maxDate: moment().startOf('day').subtract('day', 1)
+  },
+  {
+    label: 'Earlier this Week',
+    maxDate: moment().startOf('week')
+  },
+  {
+    label: 'Last Week',
+    maxDate: moment().startOf('week').subtract('week', 1)
+  },
+  {
+    label: 'Earlier this Month',
+    maxDate: moment().startOf('month')
+  },
+  {
+    label: 'Last Month',
+    maxDate: moment().startOf('month').subtract('month', 1)
+  },
+  {
+    label: 'Earlier this Year',
+    maxDate: moment().startOf('year')
+  },
+  {
+    label: 'Last Year',
+    maxDate: moment().startOf('year').subtract('year', 1)
+  },
+  {
+    label: 'Older'
+  }
+];
+timeSlices.forEach(function(slice) {
+  slice.documents = [];
+});
+
+module.exports = function sliceInTime(documents) {
+  documents.forEach(function(doc) {
+    var creationDate = moment(doc.creationDate);
+    var found = false;
+    for (var i = 0; i < timeSlices.length && !found; i+=1) {
+      if (i === 0 && creationDate.isAfter(timeSlices[i].maxDate)) {
+        found = true;
+        timeSlices[i].documents.push(doc);
+      }
+
+      if(!found && (!timeSlices[i].maxDate || creationDate.isAfter(timeSlices[i].maxDate))) {
+        found = true;
+        timeSlices[i].documents.push(doc);
+      }
+    }
+  });
+
+  return timeSlices;
+};
