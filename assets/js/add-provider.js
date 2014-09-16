44c1d5946907ebf20445fad08ee41cd03a2ccefb
Matthieu Bacconnier <neamar@neamar.fr>
2014-09-16 12:39:02 +0200

    Update add-provider.js

--- assets/js/add-provider.js
+++ assets/js/add-provider.js
@@ -11,7 +11,7 @@ var config = require('./configuration.js');
 
 async.waterfall([
   function loadSettings(cb) {
-    // Load the user settings to check retrieve the token
+    // Load the user settings to retrieve the token
     config.loadUserSettings(cb);
   },
   function callGetMarketplace(cb) {
