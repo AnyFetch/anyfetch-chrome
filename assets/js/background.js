e7598c7a861bd223e02094e326277b2056494bc6
bolvy_a <bolvy_a@epitech.eu>
2014-09-18 17:49:48 +0200

    Fix disappearing pageAction

--- assets/js/background.js
+++ assets/js/background.js
@@ -5,20 +5,27 @@ var config = require('./configuration.js');
 /**
  * Show pageAction when tab URL matches supportedSites.url regex
  */
-var checkForSupportedSite = function checkForSupportedSite(tabId, changeInfo, tab) {
-  console.log(new Date() + ': Tab updated event fired');
+function checkForSupportedSite(tab) {
   var site;
   for(var siteName in config.supportedSites) {
     site = config.supportedSites[siteName];
     if(tab.url.match(site.url)) {
-      console.log(new Date() + ': Shown pageAction for ' + siteName);
-      chrome.pageAction.show(tabId);
+      chrome.pageAction.show(tab.id);
       return;
     }
   }
-  console.log(new Date() + ': Hidden pageAction');
-  chrome.pageAction.hide(tabId);
-};
+  chrome.pageAction.hide(tab.id);
+}
+
+/**
+ * Show pageAction when tab URL matches supportedSites.url regex
+ */
+function handleOnUpdated(tabId, changeInfo, tab) {
+  if(changeInfo.status === 'loading' && tab.id && tab.url) {
+    checkForSupportedSite(tab);
+  }
+}
+
 
 // When the extension is installed or upgraded
 chrome.runtime.onInstalled.addListener(function(details) {
@@ -28,7 +35,7 @@ chrome.runtime.onInstalled.addListener(function(details) {
     // open first run page on install
     chrome.tabs.create({url: 'views/first-run.html'});
   }
-
-  // listen for tabs url changes
-  chrome.tabs.onUpdated.addListener(checkForSupportedSite);
 });
+
+// listen for tabs url changes
+chrome.tabs.onUpdated.addListener(handleOnUpdated);
