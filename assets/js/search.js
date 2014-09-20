48360b0f89f4d5e7ce4fee9f3d8544466110db25
bolvy_a <bolvy_a@epitech.eu>
2014-09-20 19:48:37 +0200

    Fix multiple searchs running at the same time

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,22 +6,32 @@ var getDocuments = require('./fetch/get-documents.js');
 var sliceInTime = require('./helpers/slice-in-time.js');
 var generateQuery = require('./context-utils.js').generateQuery;
 
+var currentSearch = '';
+
+function displayResults(search, documents, totalCount) {
+  // Order documents by time periods
+  var timeSlices = sliceInTime(documents);
+
+  // Update view
+  view.showResults(search, timeSlices, totalCount);
+}
 
 module.exports = function(context) {
   var search = generateQuery(context);
+  currentSearch = search;
 
   // Retrieve documents
   getDocuments(search, function(err, documents, totalCount) {
+    if(currentSearch !== search) {
+      // Not the last search
+      return;
+    }
     if(err) {
       return errors.show(err);
     }
     if(!totalCount) {
       errors.noResultsShow();
     }
-    // Order documents by time periods
-    var timeSlices = sliceInTime(documents);
-
-    // Update view
-    view.showResults(search, timeSlices, totalCount);
+    displayResults(search, documents, totalCount);
   });
 };
