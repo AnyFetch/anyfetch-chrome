b3a4a1e37a6a1eac96212d3239e25ec4a145fc41
Antoine Bolvy <antoine.bolvy@gmail.com>
2014-12-03 14:27:17 +0100

    Refactor popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,66 +1,78 @@
 'use strict';
 
-require('zepto/zepto.min.js');
+var async = require('async');
+
 var config = require('./config/configuration.js');
+var errors = require('./helpers/errors.js');
+var postUpdateIfNecessary = require('./fetch/post-update-if-necessary.js');
+var view = require('./popup/view.js');
+var detectContext = require('./popup/detect-context.js');
+var search = require('./popup/search.js');
 
-// We need to wait on `chrome.storage` to load the user's settings
-// before doing anything else
-config.loadUserSettings(function() {
-  var errors = require('./helpers/errors.js');
-
-  var detectContext = require('./popup/detect-context.js');
-  var view = require('./popup/view.js');
-  var postUpdateIfNecessary = require('./fetch/post-update-if-necessary.js');
-  var search = require('./popup/search.js');
+document.addEventListener('DOMContentLoaded', function() {
+  var timeout;
 
   // TODO: cache results
   // TODO: add "Still indexing" warning (use GET / for server time and GET /provider for last hydrater status)
   // TODO: i18n
+  // TODO: Get a proper error handling
+  async.waterfall([
+    // We need to wait on `chrome.storage` to load the user's settings
+    // before doing anything else
+    function loadSettings(cb) {
+      config.loadUserSettings(cb);
+    },
+    function init(cb) {
+      // Dynamically set manager url for no results
+      document.getElementById('manager-url').setAttribute('href', config.managerUrl + '/providers');
 
-  var main = function() {
-    // Dynamically set manager url for no results
-    document.getElementById('manager-url').setAttribute('href', config.managerUrl + '/providers');
+      if(!config.token) {
+        errors.showSetupAccountError();
+        return;
+      }
 
-    if(!config.token) {
-      errors.showSetupAccountError();
-      return;
-    }
-
-    // Post update
-    postUpdateIfNecessary();
-
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      var tab = tabs[0];
+      // Post update
+      postUpdateIfNecessary();
+      cb();
+    },
+    function getCurrentTab(cb) {
       // Detect context for the current tab
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        return cb(null, tabs[0]);
+      });
+    },
+    function getContext(tab, cb) {
+      timeout = setTimeout(function() {
+      view.showSpinner("Searching...");
+        timeout = setTimeout(function() {
+          view.showSpinner("Still searching...");
+          timeout = setTimeout(function() {
+            view.showSpinner("Still waiting, but something is not right.");
+          }, 10000);
+        }, 5000);
+      }, 500);
+
       detectContext(tab, function(err, context) {
         if(err) {
           return errors.show(err);
         }
-        if(!context.length) {
-          return errors.show('No context detected.');
-        }
-        view.showContext(context);
-
-        var timeout = setTimeout(function() {
-          view.showSpinner("Searching...");
-          timeout = setTimeout(function() {
-            view.showSpinner("Still searching...");
-            timeout = setTimeout(function() {
-              view.showSpinner("Still waiting, but something is not right.");
-            }, 10000);
-          }, 5000);
-        }, 500);
-
-        search(context, function(err) {
-          if(err) {
-            console.error(err);
-            return;
-          }
-          clearTimeout(timeout);
-        });
+        cb(null, context);
       });
-    });
-  };
+    },
+    function showContextAndSearch(context, cb) {
+      if(!context.length) {
+        return errors.show('No context detected.');
+      }
+      view.showContext(context);
 
-  $(document).ready(main);
+      search(context, function(err) {
+        if(err) {
+          console.error(err);
+          return;
+        }
+        clearTimeout(timeout);
+        cb();
+      });
+    }
+  ]);
 });
