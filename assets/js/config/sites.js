2538429d0449baeeb5dc80679318ad2fc86ebcdb
Antoine Bolvy <antoine.bolvy@gmail.com>
2014-12-10 14:55:34 +0100

    Not browserify compatible

--- assets/js/config/sites.js
+++ assets/js/config/sites.js
@@ -1,5 +1,19 @@
 "use strict";
 
-var autoload = require('auto-load');
-
-module.exports = autoload(__dirname + '/sites');
+module.exports = {
+  baseCrmContacts: require('./sites/base-crm-contacts.js'),
+  baseCrmDeals: require('./sites/base-crm-deals.js'),
+  baseCrmLeads: require('./sites/base-crm-leads.js'),
+  facebook: require('./sites/facebook.js'),
+  github: require('./sites/github.js'),
+  gmail: require('./sites/gmail.js'),
+  googleCalendar: require('./sites/google-calendar.js'),
+  googleContact: require('./sites/google-contact.js'),
+  googleContactOnGmail: require('./sites/google-contact-on-gmail.js'),
+  google: require('./sites/google.js'),
+  googlePlus: require('./sites/google-plus.js'),
+  linkedin: require('./sites/linkedin.js'),
+  salesforceContact: require('./sites/salesforce-contact.js'),
+  twitter: require('./sites/twitter.js'),
+  viadeo: require('./sites/viadeo.js'),
+};
