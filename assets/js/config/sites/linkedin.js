33ea582d3e22b29db0fc364a865ca385085cbedf
Antoine Bolvy <antoine.bolvy@gmail.com>
2014-12-11 19:41:15 +0100

    Fix linkedin

--- assets/js/config/sites/linkedin.js
+++ assets/js/config/sites/linkedin.js
@@ -3,13 +3,14 @@
 module.exports = {
   tests: {
     urls: [
-      'https://www.linkedin.com/profile/view?id=246055775'
+      'https://www.linkedin.com/profile/view?id=246055775',
+      'https://www.linkedin.com/pub/matthieu-bacconnier/6a/4a1/47b'
     ],
     titles: {
       'Matthieu Bacconnier | LinkedIn': ['Matthieu Bacconnier'],
     }
   },
-  url: /linkedin\.com\/profile\//,
+  url: /linkedin\.com\/(?:pub|profile)\//,
   context: {
     title: /^(.+)\s\|\sLinkedIn$/i
   }
