2b412f4a9b94c88a1dcecf4e6828b6de7ce27739
Antoine Bolvy <antoine.bolvy@gmail.com>
2014-12-29 21:58:37 +0100

    Slideshowize how-to-use page. Fix typos and rewrite some text

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ var paths = {
       'assets/js/background.js',
       'assets/js/popover.js',
       'assets/js/first-run.js',
+      'assets/js/how-to-use.js',
       'assets/js/advanced-settings.js',
       'assets/js/settings.js',
       'assets/js/oauth-callback.js',
