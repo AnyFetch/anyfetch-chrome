69a06c52fa70caa524ea592f5519a508680eac18
Amoki <hugo.duroux@gmail.com>
2015-01-23 11:19:54 +0100

    some improvments in comments/code

--- assets/js/anyfetch/get-count.js
+++ assets/js/anyfetch/get-count.js
@@ -4,7 +4,7 @@ var config = require('../config/index.js');
 var call = require('./call.js');
 
 /**
- * Caching the last query and le tast response to avoid calling multiple times api with the same query.
+ * Caching the last query and the last response to avoid calling the api multiple times with the same query
  */
 var lastQuery;
 var lastResponse;
@@ -17,11 +17,9 @@ var lastResponse;
  */
 module.exports = function getCount(query, cb) {
   if(query === lastQuery) {
-    console.log("Reuse cached query");
-    cb(null, lastResponse && lastResponse.count);
+    return cb(null, lastResponse && lastResponse.count);
   }
   else {
-    console.log("NO Reuse cached query: old query=", lastQuery);
     var options = {
       url: config.apiUrl + '/documents',
       data: {
