47220dcaa4b6b2f262e09556c3860a50e7a783d1
antonhansel <antoninribeaud@gmail.com>
2015-02-19 16:54:34 +0100

    Using the store

--- assets/js/oauth/oauth-start.js
+++ assets/js/oauth/oauth-start.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var config = require('../config/index.js');
-var saveUserData = require('../anyfetch/save-user-data.js');
 
 /*
  * Start oauth flow with remote server
@@ -28,19 +27,12 @@ module.exports = function(cb, url) {
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
       if(window.tabs[0].id === tab.id && changeInfo.url && changeInfo.url.substring(0, changeInfo.url.lastIndexOf('/')) === 'http://chrome.anyfetch.com') {
         var params = changeInfo.url.substring(changeInfo.url.lastIndexOf('=') + 1, changeInfo.url.length);
-
-        chrome.storage.sync.set({token: params}, function() {
-          saveUserData(function(err) {
-            if(err) {
-              console.error(err);
-            }
-            else {
-              success = true;
-            }
-            // destroying the popup window
-            chrome.windows.remove(window.id);
-            cb(null);
-          });
+        config.store.loadSettings(function() {
+          success = true;
+          //Closing the window after setting success to true to trigger the listener set before
+          chrome.windows.remove(window.id);
+          config.store.token = params;
+          cb(null);
         });
       }
     });
