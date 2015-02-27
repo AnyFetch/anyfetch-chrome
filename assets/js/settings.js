03bef38ebac8779db1e5b98d5ee5bad1ae67a09a
antonhansel <antoninribeaud@gmail.com>
2015-02-27 15:35:07 +0100

    Opening marketplace in new window

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -4,6 +4,17 @@ var config = require('./config/index.js');
 var checkToken = require('./helpers/check-token.js');
 
 document.addEventListener('DOMContentLoaded', function() {
+  var url = "https://manager.anyfetch.com/marketplace";
+
+  document.getElementById('open-marketplace').addEventListener('click', function() {
+    chrome.windows.create({
+      url: url,
+      type: 'popup',
+      width: 800,
+      height: 800
+    }, null);
+  });
+
   config.store.loadSettings(function() {
     checkToken(config.store.token, function(err) {
       if(err) {
@@ -12,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
       else {
         if(config.store.email) {
           document.getElementById('open-marketplace').classList.remove('hidden');
-          document.getElementById('open-marketplace').href = "https://manager.anyfetch.com/sign_in?redirection=%2Fmarketplace&email=" + config.store.email;
+          url = "https://manager.anyfetch.com/sign_in?redirection=%2Fmarketplace&email=" + config.store.email;
         }
       }
     });
